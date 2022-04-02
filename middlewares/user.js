864a7ebf5edabb88c661aeb588c9729270dc30c4
Akash Krishna <akash.grep@gmail.com>
2022-04-02 12:14:02 +0530

    add isLoggedIn middleware

---
+++ middlewares/user.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+const BigPromise = require('../middlewares/bigPromise');
+const CustomError = require('../utils/customError');
+
+const jwt = require('jsonwebtoken');
+
+exports.isLoggedIn = BigPromise(async (req, res, next)=>{
+    const token = req.cookies.token || req.header('Authorization').replace('Bearer', '');
+
+    if(!token){
+        return next(new CustomError('Login first to access this route', 401));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // we are creating this user property in req it can be named anything we want
+    req.user = await User.findById(decoded.id);
+
+    next();
+})
